ff2b74fec37047c2249277475eca42807fdb6d9e
meliance <melakmu66@gmail.com>
2024-02-20 23:25:20 +0300

    1 big o

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,27 +2,28 @@
 
 /**
  * bubble_sort - sort list with bubble
- * @array: The array to be printed
+ * @array: The array to be sorted
  * @size: Number of elements in @array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t n = 0, t = size;
-	int holder;
+    size_t i, j;
+    int temp;
 
-	if (array == NULL)
-		return;
-	for (t = size; t > 0; t--)
-	{
-		for (n = 0; n < size - 1; n++)
-		{
-			if (array[n] > array[n + 1])
-			{
-				holder = array[n];
-				array[n] = array[n + 1];
-				array[n + 1] = holder;
-				print_array(array, size);
-			}
-		}
-	}
+    if (array == NULL)
+        return;
+
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
 }
+
