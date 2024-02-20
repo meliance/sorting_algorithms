80c9334144c4a0dbeace0afe13aa349845f78a59
meliance <melakmu66@gmail.com>
2024-02-20 23:47:04 +0300

    error fixed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,31 +2,33 @@
 
 /**
  * selection_sort - sort list with selection_sort
- * @array: The array to be printed
+ * @array: The array to be sorted
  * @size: Number of elements in @array
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, c, less, tmp, swap;
+    size_t i, j, min_idx;
+    int temp;
 
-	if (array == NULL)
-		return;
-	for (i = 0; i < size; i++)
-	{
-		for (less = i, c = i; c < size; c++)
-			if (array[c] < array[less])
-			{
-				less = c;
-				swap = 1;
-			}
-		if (swap == 1)
-		{
-			tmp = array[less];
-			array[less] = array[i];
-			array[i] = tmp;
-			print_array(array, size);
-			swap = 0;
-		}
-	}
+    if (array == NULL)
+        return;
+
+    for (i = 0; i < size - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_idx])
+                min_idx = j;
+        }
+        if (min_idx != i)
+        {
+            // Swap
+            temp = array[min_idx];
+            array[min_idx] = array[i];
+            array[i] = temp;
+            print_array(array, size);
+        }
+    }
 }
 
