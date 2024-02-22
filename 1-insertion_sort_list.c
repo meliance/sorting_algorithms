2fb0d131c09e2eda723ea01b1fd7b26864df7dab
meliance <melakmu66@gmail.com>
2024-02-23 01:39:33 +0300

    fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,31 +1,33 @@
 #include "sort.h"
-
 /**
- * insertion_sort_list - insertion sorting algorithm
- * @list: linked list to sort
- */
+* insertion_sort_list - insertion sorting algorithm
+* @list: linked list to sort
+*/
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *temp;
-    int n;
+	listint_t *tmp;
+	int n;
 
-    if (!list || !*list)
-        return;
-
-    current = *list;
-
-    while (current)
-    {
-        temp = current;
-        while (temp->prev && temp->n < temp->prev->n)
-        {
-            n = temp->n;
-            temp->n = temp->prev->n;
-            temp->prev->n = n;
-            temp = temp->prev;
-            print_list(*list);
-        }
-        current = current->next;
-    }
+	if (!list)
+		return;
+	tmp = *list;
+	while (tmp)
+	{
+		while (tmp)
+		{
+			if (tmp->next)
+			{
+				if (tmp->n > tmp->next->n)
+				{
+					n = tmp->n;
+					*(int *)&tmp->n = tmp->next->n;
+					*(int *)&tmp->next->n = n;
+					tmp = *list;
+					print_list(*list);
+					break;
+				}
+			}
+			tmp = tmp->next;
+		}
+	}
 }
-
